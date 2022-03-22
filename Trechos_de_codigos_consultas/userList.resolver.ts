9b54b330ae96000321428cd0b6d75d0a262ce668
rogerioTadeuDosReis <rogerio.rtr46@gmail.com>
2022-03-22 08:43:58 -0300

    Adicionado exemplo de resolver em angular

---
+++ Trechos_de_codigos_consultas/userList.resolver.ts
@@ -0,0 +1,35 @@
+/**
+ * Exemplo de arquivo resolver para ser implementado em qualquer projeto angular
+ * 
+ * Motivação: carregar os dados antes da renderização do componente no template
+ * 
+ * Não esquecer de fazer os devidos imports
+ */
+
+@Injectable({ providedIn: 'root'})
+export class UserListResolver implements Resolve<Observable<User[]>> {
+
+    constructor(private service: UserService){}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<User[]>{
+        const userName = route.params.userName;
+        return this.service.listFromUser(userName);
+    }
+}
+
+/**
+ * configurar o resolve dentro do arquivo routing.module.ts
+ */
+
+resolve: {
+    users: UserListResolver
+}
+
+/**
+ * no componente dentro do init chamar da seguinte forma
+ */
+
+users: User[] = [];
+
+this.users = this.activatedRoute.snapshot.data.users; ou 
+this.users = this.activatedRoute.snapshot.data['users'];
